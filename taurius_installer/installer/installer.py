0bcd40d70c65eef903b9c8fc1ce451c41043f695
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-06-30 23:11:29 -0300

    feat: installer class and tests

---
+++ taurius_installer/installer/installer.py
@@ -0,0 +1,36 @@
+import shutil
+import zipfile
+from pathlib import Path
+
+
+class Installer:
+    def __init__(self, filepath: Path, destiny_installation: Path):
+        if not filepath.exists():
+            raise FileNotFoundError(filepath)
+
+        if not destiny_installation.exists():
+            destiny_installation.parent.mkdir(parents=True, exist_ok=True)
+
+        self._origin = filepath
+        self._destiny = destiny_installation
+
+        self._setup_folders()
+
+    def _setup_folders(self):
+        self._origin_folder = self._origin.parent.absolute()
+        self._destiny_folder = self._destiny.parent.absolute()
+
+    def _move_file_to_installation_path(self):
+        shutil.copy2(self._origin, self._destiny)
+
+    def _clear_installation(self):
+        self._origin.unlink()
+        self._destiny.unlink()
+
+    def install(self):
+        self._move_file_to_installation_path()
+
+        with zipfile.ZipFile(self._origin, "r") as executable:
+            executable.extractall(self._destiny_folder)
+
+        self._clear_installation()
---
+++ tests/installer/test_install_executable.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+from pathlib import Path
+
+from taurius_installer.installer.installer import Installer
+from tests.generics import CommonTestCase
+
+
+class TestInstallExecutable(CommonTestCase):
+    def setUp(self):
+        self.main_executable_filename = "electron.vite.config.ts"
+        self.basepath = Path(os.getcwd())
+        self.executable_filepath = (
+            self.basepath / "tests" / "files" / "rotion-0.0.3.zip"
+        )
+        self.executable_filename = f"{self.executable_filepath.stem} (test-installer){self.executable_filepath.suffix}"
+        self.executable_filepath_copy = (
+            self.basepath / "tests" / "files" / self.executable_filename
+        )
+
+        shutil.copy2(self.executable_filepath, self.executable_filepath_copy)
+
+        self.installation_folder = self.basepath / "test-installation"
+        self.installation_executable_filepath = (
+            self.installation_folder / self.executable_filename
+        )
+
+        self.installer = Installer(
+            self.executable_filepath_copy, self.installation_executable_filepath
+        )
+
+    def tearDown(self):
+        if self.installation_folder.exists():
+            shutil.rmtree(self.installation_folder)
+
+    def test_install_executable_on_specified_folder(self):
+        self.installer.install()
+        executable_folder = (
+            self.installation_folder / self.executable_filepath.stem
+        )
+
+        main_executable_filepath = (
+            executable_folder / self.main_executable_filename
+        )
+
+        self.assertTrue(executable_folder.exists())
+        self.assertTrue(main_executable_filepath.exists())
